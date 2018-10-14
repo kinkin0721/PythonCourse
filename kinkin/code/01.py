c5230294a2d064be0526a32f4662a5ed52a6d64b
jinsheng <jinsheng@jibiao.org>
2018-10-14 23:44:52 +0800

    首次上传

---
+++ kinkin/code/01.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+
+
+def fibonacci(n):
+    if n == 0:
+        print(0)
+    elif n == 1:
+        print(1)
+    else:
+        n1 = 0
+        n2 = 1
+        count = 1
+        while count < n:
+            nth = n1 + n2
+            n1 = n2
+            n2 = nth
+            count += 1
+        print(nth)
+
+
+def find2(n):
+
+    if n < 0:
+        print("请输入正整数")
+
+    a = [9, 0, 8, 0, 8, 2, 7, 6, 2, 1, 2, 2, 10, 39]
+
+    count = 0
+    count_for_2 = 0
+    for num in a:
+        if num == 2:
+            count_for_2 += 1
+            if count_for_2 == n:
+                break
+        count += 1
+
+    if count_for_2 < n:
+        print("meiyounameduo2 godie")
+    else:
+        print(count)
+
+
+if __name__ == "__main__":
+    print("Hello world!")
+
+    # count = 4
+    # count = 999
+    #
+    # if count == 4:
+    #     print("chenggong")
+    # else:
+    #     print("shibai")
+    #
+    # if count != 4:
+    #     print("chenggong")
+    # else:
+    #     print("shibai")
+    #
+    # isMonday = True
+    # isSunday = False
+
+    # [ismonday, is]
+    # today = [True, False, False, False, False, False, False]
+    #
+    # for index in range(100):
+    #     print(today[index])
+    #
+    # count = 0
+    # for day in today:
+    #     print(count)
+    #     count = count + 1
+    #     count += 1
+
+    # countsum = 0
+    # for idx in range(-1, 101):
+    #     sum = (sum + idx) / 2
+    #
+    # print(sum)
+
+    # count = 0
+    # while count >= 0:
+    #     count = count + 1
+    #     print(count)
+
+    numList = [2, 3, 4, 5, 7, 9, 10, 100]
+
+    # find2(90)
+
+    fibonacci(8)
+
+    d = {'apple': 3, 'banana': 10, 'pineapple': 7}
+
+    print(numList)
+
+
+
+
+
+
+
+
+
+
+
+
+
